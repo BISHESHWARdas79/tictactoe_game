d9e534003504468a5a4cfd910390796082fe629b
Bisheshwar Das <bisheshwardas79@gmail.com>
2023-03-28 00:06:55 +0530

    added 'Start New Game' feature

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import StatusMessage from './Components/statusMessage.jsx';
 import { useState } from 'react';
 import { calculateWinner } from './winner.js';
 
+const NEW_GAME = [{ squares: Array(9).fill(null), isXnext: false }];
+
 function App() {
-  const [history, setHistory] = useState([
-    { squares: Array(9).fill(null), isXnext: false },
-  ]);
+  const [history, setHistory] = useState(NEW_GAME);
   // const [squares, setsquares] = useState(Array(9).fill(null));
   // const [isXnext, setIsXNext] = useState(false);
   const [currentMove, setCurrentMove] = useState(0);
@@ -55,6 +55,11 @@ function App() {
     setCurrentMove(move);
   };
 
+  const onNewGAme = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
+
   return (
     <div className="app">
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
@@ -62,6 +67,13 @@ function App() {
         squares={gamingBoard.squares}
         handleSquareClick={handleSquareClick}
       />
+      <button
+        className={`btn-reset ${winner ? 'active' : ''}`}
+        type="button"
+        onClick={onNewGAme}
+      >
+        START NEW GAME
+      </button>
       <h2>Current Game History</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
