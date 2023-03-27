b64783af238bd7b7068a5efda533ad1b72bbc27c
Bisheshwar Das <bisheshwardas79@gmail.com>
2023-03-27 17:25:18 +0530

    learnt about state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import Board from './Components/Board.jsx';
 import './style.scss';
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  const onBtnClick = () => {
+    // setCounter(counter + 1);
+    setCounter(currentCounter => {
+      return currentCounter + 1;
+    });
+  };
   return (
     <div className="app">
-      <Board />
+      <div>
+        <button onClick={onBtnClick}>CLick me Please</button>
+        <div>{counter}</div>
+      </div>
     </div>
   );
 }
