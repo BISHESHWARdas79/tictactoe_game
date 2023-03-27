93cc132d2c7b48fb5742457aecf574d12bd07e82
Bisheshwar Das <bisheshwardas79@gmail.com>
2023-03-28 02:06:16 +0530

    Added deploy script

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,9 @@ function App() {
 
   return (
     <div className="app">
+      <h1>
+        TIC <span className="text-orange">TAC</span> TOE
+      </h1>
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
       <Board
         squares={gamingBoard.squares}
@@ -74,7 +77,13 @@ function App() {
       >
         START NEW GAME
       </button>
-      <h2>Current Game History</h2>
+      <h2
+        style={{
+          fontWeight: 'normal',
+        }}
+      >
+        Current Game History
+      </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
