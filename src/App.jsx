43d6f5f3e8b87774f062191f25a88ec7771e7817
Bisheshwar Das <bisheshwardas79@gmail.com>
2023-03-27 17:54:47 +0530

    handle what happens on click

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,9 @@ import Board from './Components/Board.jsx';
 import './style.scss';
 
 function App() {
-  const [counter, setCounter] = useState(1);
-
-  const onBtnClick = () => {
-    // setCounter(counter + 1);
-    setCounter(currentCounter => {
-      return currentCounter + 1;
-    });
-  };
   return (
     <div className="app">
-      <div>
-        <button onClick={onBtnClick}>CLick me Please</button>
-        <div>{counter}</div>
-      </div>
+      <Board />
     </div>
   );
 }
